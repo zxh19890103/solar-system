05ababe8ca73adeb91d4812f7f2311eea58cc902
zxh <zhangxinghai79@gmail.com>
2021-08-02 22:57:06 +0800

    ai

--- sys-n/bootstrap/solar-data.ts
+++ sys-n/bootstrap/solar-data.ts
@@ -36,33 +36,27 @@ import * as IMAGES from '../../planets-inf/images'
 import { BOOTSTRAP_STATE } from "../jpl-data"
 import { point, sphere } from "../providers"
 
-const mercurySystemActive = false
 const mercurySystem: CelestialSystem = {
-  hidden: !mercurySystemActive,
   path: true,
   body: Mercury,
   bootstrapState: BOOTSTRAP_STATE.Mercury
 }
 
-const venusSystemActive = false
 const venusSystem: CelestialSystem = {
-  hidden: !venusSystemActive,
   body: Venus,
   path: true,
   bootstrapState: BOOTSTRAP_STATE.Venus
 }
 
-const earthSystemActive = true
 const earthSystem: CelestialSystem = {
-  hidden: !earthSystemActive,
   body: Earth,
   bootstrapState: BOOTSTRAP_STATE.Earth,
   path: false,
+  provider: sphere,
   subSystems: [
     {
       body: Luna,
       provider: sphere,
-      hidden: true,
       moon: true,
       path: false,
       bootstrapState: BOOTSTRAP_STATE.Luna
@@ -70,9 +64,7 @@ const earthSystem: CelestialSystem = {
   ]
 }
 
-const marsSystemActive = false
 const marsSystem: CelestialSystem = {
-  hidden: !marsSystemActive,
   body: { ...Mars, map: IMAGES.K_8K_MARS_JPG },
   bootstrapState: BOOTSTRAP_STATE.Mars,
   path: false,
@@ -81,50 +73,43 @@ const marsSystem: CelestialSystem = {
   subSystems: [
     {
       body: Phobos,
-      hidden: false,
       moon: true,
       path: true,
       provider: point,
       bootstrapState: BOOTSTRAP_STATE.Phobos
     },
-    { body: Deimos, hidden: true, moon: true, }
+    { body: Deimos, moon: true, }
   ]
 }
 
-const jupiterSystemActive = false
 const jupiterSystem: CelestialSystem = {
-  hidden: !jupiterSystemActive,
   body: Jupiter,
   bootstrapState: BOOTSTRAP_STATE.Jupiter,
   subSystems: [
-    { body: Lo, hidden: true, moon: true, },
-    { body: Europa, hidden: true, moon: true, },
-    { body: Ganymede, hidden: true, moon: true, },
-    { body: Callisto, hidden: true, moon: true, }
+    { body: Lo, moon: true, },
+    { body: Europa, moon: true, },
+    { body: Ganymede, moon: true, },
+    { body: Callisto, moon: true, }
   ]
 }
 
-const saturnSystemActive = true
 const saturnSystem: CelestialSystem = {
-  hidden: !saturnSystemActive,
   body: { ...Saturn, map: IMAGES.K_8K_SATURN_JPG },
   bootstrapState: BOOTSTRAP_STATE.Saturn,
   provider: sphere,
   subSystems: [
     {
-      hidden: false,
       body: Titan,
       moon: true,
       bootstrapState: BOOTSTRAP_STATE.Titan
     },
     {
-      hidden: false,
       body: Rhea,
       moon: true,
       bootstrapState: BOOTSTRAP_STATE.Rhea
     },
     {
-      hidden: false,
+
       body: Enceladus,
       moon: true,
       bootstrapState: BOOTSTRAP_STATE.Enceladus
@@ -132,16 +117,14 @@ const saturnSystem: CelestialSystem = {
   ]
 }
 
-const neptuneSystemActive = false
 const neptuneSystem: CelestialSystem = {
-  hidden: !neptuneSystemActive,
   body: { ...Neptune, map: IMAGES.K_2K_NEPTUNE_JPG },
   bootstrapState: BOOTSTRAP_STATE.Neptune,
   path: false,
   provider: sphere,
+  rotates: true,
   subSystems: [
     {
-      hidden: false,
       moon: true,
       path: true,
       body: Nereid,
@@ -150,22 +133,23 @@ const neptuneSystem: CelestialSystem = {
   ]
 }
 
-const uranusSystemActive = false
 const uranusSystem: CelestialSystem = {
-  hidden: !uranusSystemActive,
-  body: Uranus,
+  provider: sphere,
+  rotates: true,
+  body: { ...Uranus, map: IMAGES.K_2K_URANUS_JPG },
   bootstrapState: BOOTSTRAP_STATE.Uranus
 }
 
-const plutoSystemActive = false
 const plutoSystem: CelestialSystem = {
-  hidden: !plutoSystemActive,
-  body: Pluto,
+  provider: sphere,
+  rotates: true,
+  body: { ...Pluto, map: IMAGES.MAPS_PLUTO_2048X1024_JPG },
   bootstrapState: BOOTSTRAP_STATE.Pluto,
   subSystems: [
     {
       moon: true,
       body: Charon,
+      provider: point,
       bootstrapState: BOOTSTRAP_STATE.Charon
     }
   ]
@@ -173,24 +157,20 @@ const plutoSystem: CelestialSystem = {
 
 const comets: CelestialSystem[] = [
   {
-    hidden: true,
     body: Halley,
     bootstrapState: BOOTSTRAP_STATE.Halley,
   },
   {
-    hidden: true,
     path: true,
     tail: false,
     body: Tempel1,
     bootstrapState: BOOTSTRAP_STATE.Tempel1
   },
   {
-    hidden: true,
     body: Holmes,
     bootstrapState: BOOTSTRAP_STATE.Holmes
   },
   {
-    hidden: true,
     body: HaleBopp
   }
 ]
@@ -198,7 +178,6 @@ const comets: CelestialSystem[] = [
 export const FAR_OF_CAMERA: THREE.Vector3Tuple = [0, 2 * AU, 0]
 
 export const system: CelestialSystem = {
-  hidden: false,
   body: Sun,
   bootstrapState: BOOTSTRAP_STATE.Sol,
   subSystems: [
@@ -215,8 +194,17 @@ export const system: CelestialSystem = {
   ]
 }
 
+type SystemNames = 'Sun' | 'Earth' | 'Mars' | 'Mercury' | 'Venus' | 'Jupiter' | 'Saturn' | 'Uranus' | 'Neptune' | 'Pluto' | 'Halley' | 'Tempel1' | 'Holmes' | 'HaleBopp' | 'Charon' | 'Nereid' | 'Enceladus' | 'Rhea' | 'Titan' | 'Lo' | 'Europa' | 'Ganymede' | 'Callisto' | 'Phobos' | 'Deimos' | 'Luna'
+type SystemOn = Array<SystemNames>
+let onoffs: SystemOn = []
+export const setSystemsActive = (options: SystemOn) => {
+  onoffs = options
+}
+
 export const initializeSystem = (sys: CelestialSystem, parent: CelestialSystem) => {
-  if (sys.hidden || !sys.bootstrapState) return
+  if (!onoffs.includes(sys.body.name as SystemNames)) return
+  if (sys.hidden === true) return
+  if (!sys.bootstrapState) return
   sys.celestialBody = new CelestialBody(sys)
 
   if (parent) {
--- sys-n/bootstrap/solar.ts
+++ sys-n/bootstrap/solar.ts
@@ -1,40 +1,42 @@
 import * as THREE from 'three'
 import { CelestialBody, setVec3RelativeTo } from "../gravity"
-import { FAR_OF_CAMERA, system, initializeSystem } from './solar-data'
+import { FAR_OF_CAMERA, system, initializeSystem, setSystemsActive } from './solar-data'
 import { CAMERA_POSITION_Y } from '../settings'
 import { makeCameraEditable } from '../editor'
 import { AU } from '../../sys/constants'
 
 const bootstrap = (scene: THREE.Scene, renderer: THREE.WebGLRenderer, camera: THREE.Camera) => {
 
+  setSystemsActive(['Sun', 'Earth', 'Luna'])
   initializeSystem(system, null)
 
   const star = system.celestialBody
   star.init(scene)
   const next = CelestialBody.createUniNextFn(star)
 
-  const sunLight = new THREE.PointLight(0xffffff, .4)
+  const sunLight = new THREE.PointLight(0xffffff, 1)
   sunLight.position.set(0, 0, 0)
   scene.add(sunLight)
-  const ambientLight = new THREE.AmbientLight(0xffffff, .3)
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1)
   scene.add(ambientLight)
 
-  // const mars = star.find('Mars')
-  // const neptune = star.find('Neptune')
-  const saturn = star.find('Saturn')
-  const earth = star.find('Earth')
-  // const moon = earth.find('Luna')
+  renderer.physicallyCorrectLights = true
 
-  // sunLight.distance = star.position.distanceTo(saturn.position)
+  const mars = star.find('Mars')
+  const neptune = star.find('Neptune')
+  const uranus = star.find('Uranus')
+  const pluto = star.find('Pluto')
+  const earth = star.find('Earth')
+  const luna = earth.find('Luna')
 
   camera.up.set(0, 1, 0)
   camera.position.set(0, 0, 0)
-  earth.o3.add(camera)
+  luna.o3.add(camera)
 
   const animate = () => {
     requestAnimationFrame(animate)
     next()
-    camera.lookAt(...saturn.positionArr)
+    camera.lookAt(...earth.positionArr)
     renderer.render(scene, camera)
   }
   animate()
