129fadc92c6d537a54fbf1646408219c2447c8a6
singhi <zhangxinghai79@gmail.com>
2022-08-01 17:55:04 +0800

    ha

--- warehouse/idea.ts
+++ warehouse/idea.ts
@@ -1,6 +1,11 @@
 import * as THREE from "three"
+import { ArcballControls } from "./ArcballControls"
 // import L from "leaflet"
 declare var L
+// import React from "react"
+declare var React
+// import ReactDOM from "react-dom"
+declare var ReactDOM
 
 const { random } = Math
 
@@ -8,46 +13,85 @@ const randInt = (min: number, max: number) => {
   return min + random() * (max - min)
 }
 
-let _id_seed = 1998
+namespace core {
+  let _id_seed = 1998
 
-interface Constructor<M> {
-  new (...args: any[]): M
-}
+  export interface Constructor<M> {
+    new (...args: any[]): M
+  }
 
-abstract class Base<V> {
-  readonly id = _id_seed++
-  $$parent: any = null
+  export abstract class Base<V> extends L.Evented {
+    private lastSnapshot = null
+    readonly id = _id_seed++
+    $$parent: any = null
 
-  views: Set<V> = new Set()
+    views: Set<V> = new Set()
 
-  abstract fromJSON(d: any): this
+    abstract fromJSON(d: any): this
+    abstract toMyJSON(): any
 
-  requestHookCall(effect: string) {
-    for (const view of this.views) {
-      view[`on${effect}`] && view[`on${effect}`]()
+    snapshot() {
+      this.lastSnapshot = this.toMyJSON()
+    }
+
+    getLastSnapshot() {
+      return this.lastSnapshot
+    }
+
+    requestHookCall(effect: string) {
+      this.snapshot()
+      appendViewHooksCallReq({ context: this, effect })
     }
   }
-}
 
-interface BaseView<M> {
-  model: M
-  onTrash(): void
-  onUpdate?(): void
-}
+  type ViewHookCallReq = { context: Base<any>; effect: string }
 
-// this is view;
-/**
- */
-function baseViewConstruct(this: any, m: any, options: any) {
-  this.model = m
-  m.views.add(this)
-}
+  const viewHooksCallReqs = new Set<ViewHookCallReq>()
+  let isFlushScheduled = false
+  let isCallingEffects = false
 
-namespace api {
-  interface Position {
-    x: number
-    y: number
-    z: number
+  const appendViewHooksCallReq = (req: ViewHookCallReq) => {
+    viewHooksCallReqs.add(req)
+
+    if (isFlushScheduled) return
+    isFlushScheduled = true
+    queueMicrotask(flush)
+  }
+
+  const flush = () => {
+    isCallingEffects = true
+
+    for (const req of viewHooksCallReqs) {
+      const { context, effect } = req
+      const code = effect.charCodeAt(0)
+      const isCapital = code >= 65 && code <= 90
+      const method = isCapital ? `on${effect}` : effect
+      const snapshot = context.getLastSnapshot()
+      for (const view of context.views) {
+        view[method] && view[method](snapshot)
+      }
+
+      context.fire("effect", { effect })
+    }
+
+    viewHooksCallReqs.clear()
+
+    isCallingEffects = false
+    isFlushScheduled = false
+  }
+
+  export interface BaseView<M> {
+    model: M
+    onTrash(): void
+    onUpdate?(): void
+  }
+
+  // this is view;
+  /**
+   */
+  export function baseViewConstruct(this: any, m: any, options: any) {
+    this.model = m
+    m.views.add(this)
   }
 
   export interface ViewContainer<M, V> {
@@ -58,14 +102,28 @@ namespace api {
   }
 
   export type ViewMake<M, V> = (m: M) => V
+}
 
-  export class Person extends Base<PersonView> {
+namespace model {
+  interface Position {
+    x: number
+    y: number
+    z: number
+  }
+
+  export class Person extends core.Base<PersonView> {
     position: Position = { x: 0, y: 0, z: 0 }
+    size: number = 200
 
     walk() {
+      this.requestHookCall("Walk")
       this.position.x += randInt(-2000, 2000)
       this.position.y += randInt(-2000, 2000)
-      this.requestHookCall("Walk")
+    }
+
+    setSize(size: number) {
+      this.requestHookCall("bigger")
+      this.size = size
     }
 
     override fromJSON(d: any): this {
@@ -76,23 +134,30 @@ namespace api {
       }
       return this
     }
+
+    toMyJSON() {
+      return {
+        position: JSON.parse(JSON.stringify(this.position)),
+        size: this.size,
+      }
+    }
   }
 
-  export interface PersonView extends BaseView<Person> {
-    onWalk?(): void
-    bigger(): void
+  export interface PersonView extends core.BaseView<Person> {
+    onWalk?(...args: any[]): void
+    bigger(...args: any[]): void
   }
 
-  export class List<V, M extends Base<V>> extends L.Evented {
+  export class List<V, M extends core.Base<V>> extends L.Evented {
     items: Set<M> = new Set()
     index: Map<number, M> = new Map()
     size: number = 0
-    private _item_C: Constructor<M> = null
-    private containers: ViewContainer<M, V>[] = []
+    private _item_C: core.Constructor<M> = null
+    private containers: core.ViewContainer<M, V>[] = []
 
     private isBatching = false
 
-    constructor(C: Constructor<M>, items: M[]) {
+    constructor(C: core.Constructor<M>, items: M[]) {
       super()
       this._item_C = C
       this.addRange(...items)
@@ -129,9 +194,12 @@ namespace api {
       this.size += 1
 
       if (this.isBatching) return
+
       for (const c of this.containers) {
         c.add(c.$$make(m))
       }
+
+      this.fire("add", { item: m })
     }
 
     addRange(...items: M[]) {
@@ -146,6 +214,8 @@ namespace api {
       for (const c of this.containers) {
         c.add(...items.map(c.$$make))
       }
+
+      this.fire("add.range", { items: items })
     }
 
     remove(m: M) {
@@ -160,6 +230,8 @@ namespace api {
       m.requestHookCall("Trash")
 
       if (this.isBatching) return
+
+      this.fire("remove", { item: m })
     }
 
     removeRange(...items: M[]) {
@@ -170,6 +242,8 @@ namespace api {
       }
 
       this.isBatching = false
+
+      this.fire("remove.range", { items: items })
     }
 
     removeById(id: number) {
@@ -185,72 +259,153 @@ namespace api {
       }
     }
 
-    addContainer(c: ViewContainer<M, V>) {
+    addContainer(c: core.ViewContainer<M, V>) {
       this.containers.push(c)
     }
   }
 }
 
-class Person2D extends L.Circle implements api.PersonView {
-  model: api.Person
+namespace view {
+  export class Person2D extends L.Rectangle implements model.PersonView {
+    model: model.Person
 
-  constructor(person: api.Person, options: any) {
-    super([0, 0], { radius: 1000 })
-    baseViewConstruct.call(this, person, options)
+    constructor(person: model.Person, options: any) {
+      super(
+        [
+          [0, 0],
+          [0, 0],
+        ],
+        { color: "#00ff00" }
+      )
+      core.baseViewConstruct.call(this, person, options)
 
-    this.on("click", (evt) => {
-      this.model.$$parent.remove(this.model)
-    })
+      this.on("click", (evt) => {
+        this.model.$$parent.remove(this.model)
+      })
+
+      this.on("mouseover", () => {
+        this.model.setSize(400)
+      }).on("mouseout", () => {
+        this.model.setSize(200)
+      })
+    }
+
+    private setLatLng(lat: number, lng: number) {
+      const { size } = this.model
+      this.setBounds(
+        L.latLngBounds([-size + lat, -size + lng], [size + lat, size + lng])
+      )
+    }
+
+    onWalk(): void {
+      const { x, y } = this.model.position
+      this.setLatLng(y, x)
+    }
+
+    onTrash(): void {
+      this.remove()
+    }
+
+    bigger(): void {
+      const { x: lng, y: lat } = this.model.position
+      this.setLatLng(lat, lng)
+    }
   }
 
-  onWalk(): void {
-    const { x, y } = this.model.position
-    this.setLatLng(L.latLng(y, x))
+  export class Person2D2 extends L.Circle implements model.PersonView {
+    model: model.Person
+
+    constructor(person: model.Person, options: any) {
+      super([0, 0], { color: "#00ff00", radius: 1000 })
+      core.baseViewConstruct.call(this, person, options)
+    }
+
+    onWalk(...args: any[]): void {
+      const position = this.model.position
+      this.setLatLng([position.y, position.x])
+    }
+
+    bigger(...args: any[]): void {}
+
+    onTrash(): void {
+      this.remove()
+    }
   }
 
-  onTrash(): void {
-    this.remove()
+  export class Person3D extends THREE.Mesh implements model.PersonView {
+    model: model.Person
+
+    constructor(person: model.Person, options: any) {
+      super(
+        new THREE.BoxGeometry(2000, 2000, 2000, 120, 120, 120),
+        new THREE.MeshPhongMaterial({ color: 0x00ff00, specular: 0x00ffff })
+      )
+      core.baseViewConstruct.call(this, person, options)
+    }
+
+    rotate() {
+      this.rotation.x += 0.1
+      this.rotation.y += 0.1
+    }
+
+    onWalk(): void {
+      const { x, y } = this.model.position
+      this.position.set(x, y, 0)
+    }
+
+    onTrash(): void {
+      this.removeFromParent()
+    }
+
+    bigger({ size }): void {
+      const geometry = this.geometry
+      const positions = (geometry as THREE.BufferGeometry).attributes.position
+        .array as Array<number>
+
+      const scale = this.model.size / size
+
+      for (let i = 0; i < positions.length; i += 3) {
+        const v = new THREE.Vector3(
+          positions[i],
+          positions[i + 1],
+          positions[i + 2]
+        ).multiplyScalar(scale)
+        positions[i] = v.x
+        positions[i + 1] = v.y
+        positions[i + 2] = v.z
+      }
+      ;(geometry as THREE.BufferGeometry).attributes.position.needsUpdate = true
+    }
   }
 
-  bigger(): void {
-    this.setRadius(2000)
-  }
-}
+  export class Person3D2 extends THREE.Mesh implements model.PersonView {
+    model: model.Person
 
-class Person3D extends THREE.Mesh implements api.PersonView {
-  model: api.Person
+    constructor(person: model.Person, options: any) {
+      super(
+        new THREE.SphereGeometry(1000, 120, 120, 120),
+        new THREE.MeshPhongMaterial({ color: 0x00ff00, specular: 0x00ffff })
+      )
+      core.baseViewConstruct.call(this, person, options)
+    }
 
-  constructor(person: api.Person, options: any) {
-    super(
-      new THREE.BoxGeometry(2000, 2000, 2000),
-      new THREE.MeshPhongMaterial({ color: 0x00ff00 })
-    )
-    baseViewConstruct.call(this, person, options)
-  }
+    onWalk(...args: any[]): void {
+      const { x, y } = this.model.position
+      this.position.set(x, y, 0)
+    }
 
-  rotate() {
-    this.rotation.x += 0.1
-    this.rotation.y += 0.1
-  }
+    bigger(...args: any[]): void {}
 
-  onWalk(): void {
-    const { x, y } = this.model.position
-    this.position.set(x, y, 0)
+    onTrash(): void {
+      this.removeFromParent()
+    }
   }
 
-  onTrash(): void {
-    this.removeFromParent()
-  }
-
-  bigger(): void {}
-}
-
-namespace containers {
-  export class LeafletContainer<M, V> implements api.ViewContainer<M, V> {
-    $$make: api.ViewMake<M, V>
+  export class LeafletContainer<M, V> implements core.ViewContainer<M, V> {
+    $$make: core.ViewMake<M, V>
     $$value: any
 
-    constructor(value: any, make: api.ViewMake<M, V>, ...initialItems: M[]) {
+    constructor(value: any, make: core.ViewMake<M, V>, ...initialItems: M[]) {
       this.$$value = value
       this.$$make = make
 
@@ -270,14 +425,14 @@ namespace containers {
     }
   }
 
-  export class ThreeContainer<M, V> implements api.ViewContainer<M, V> {
-    $$make: api.ViewMake<M, V> = null
+  export class ThreeContainer<M, V> implements core.ViewContainer<M, V> {
+    $$make: core.ViewMake<M, V> = null
     /**
      * L.FeatureGroup, L.LayerGroup, THREE.Group, THREE.Object3D
      */
     $$value: any = null
 
-    constructor(value: any, make: api.ViewMake<M, V>, ...initialItems: M[]) {
+    constructor(value: any, make: core.ViewMake<M, V>, ...initialItems: M[]) {
       this.$$value = value
       this.$$make = make
 
@@ -297,14 +452,15 @@ namespace containers {
 }
 
 class Graph {
-  persons = new api.List<api.PersonView, api.Person>(api.Person, []).fromJSON(
-    Array(1).fill(0)
-  )
+  persons = new model.List<model.PersonView, model.Person>(
+    model.Person,
+    []
+  ).fromJSON(Array(1).fill(0))
 
   render() {
-    const c = new containers.LeafletContainer(
+    const c = new view.LeafletContainer(
       new L.FeatureGroup(),
-      (p: api.Person) => new Person2D(p, {}),
+      (p: model.Person) => new view.Person2D2(p, {}),
       ...this.persons
     )
 
@@ -313,9 +469,9 @@ class Graph {
   }
 
   render3d() {
-    const c = new containers.ThreeContainer(
+    const c = new view.ThreeContainer(
       new THREE.Group(),
-      (p: api.Person) => new Person3D(p, {}),
+      (p: model.Person) => new view.Person3D2(p, {}),
       ...this.persons
     )
 
@@ -336,13 +492,11 @@ const App = () => {
   let size = 0
 
   const loop = () => {
-    setTimeout(loop, 3000)
+    setTimeout(loop, 5)
 
-    if (size < 30 && Math.random() > 0.1) {
-      graph.persons.add(new api.Person().fromJSON(null))
+    if (size < 100 && Math.random() > 0.1) {
+      graph.persons.add(new model.Person().fromJSON(null))
       size += 1
-    } else {
-      graph.persons.removeById(0 ^ randInt(0, _id_seed))
     }
 
     for (const p of graph.persons) p.walk()
@@ -385,17 +539,23 @@ const App = () => {
     const scene = new THREE.Scene()
 
     const { clientHeight: H, clientWidth: W } = containerElement
-    const camera = new THREE.PerspectiveCamera(45, W / H, 0.1, 100000)
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      W / H,
+      0.1,
+      Number.MAX_SAFE_INTEGER
+    )
 
     const directionLight = new THREE.DirectionalLight(0xffffff, 1)
-    directionLight.position.set(0, 0, 1)
+    directionLight.position.set(0, 1, 0)
     scene.add(directionLight)
 
     const ambientLight = new THREE.AmbientLight(0xffffff, 1)
     scene.add(ambientLight)
 
-    camera.up.set(1, 0, 0)
-    camera.position.set(0, 0, 80000)
+    camera.up.set(0, 1, 0)
+    camera.position.set(0, 100000, 50000)
+    camera.lookAt(0, 0, 0)
 
     scene.add(camera)
 
@@ -409,6 +569,9 @@ const App = () => {
 
     containerElement.appendChild(renderer.domElement)
 
+    const controls = new ArcballControls(camera, renderer.domElement, scene)
+    controls.update()
+
     for (const g of graph.render3d()) {
       scene.add(g.$$value)
     }
@@ -419,6 +582,72 @@ const App = () => {
     }
 
     loop()
+  }
+
+  // dom
+  {
+    const root = document.querySelector(".dom")
+    const h = React.createElement
+
+    let seq = 0
+    const reducer = () => {
+      return seq++
+    }
+
+    const Item = (props: { person: model.Person }) => {
+      const { person } = props
+      const position = person.position
+      const [_, tick] = React.useReducer(reducer, seq)
+
+      React.useEffect(() => {
+        person.on("effect", tick)
+        return () => {
+          person.off("effect", tick)
+        }
+      }, [])
+
+      return h(
+        "li",
+        {},
+        h(
+          "span",
+          {},
+          "#" + person.id + ` xy:${0 ^ position.x},${0 ^ position.y}`
+        ),
+        h(
+          "a",
+          {
+            href: "javascript:void(0);",
+            style: { marginLeft: 10 },
+            onClick: () => {
+              graph.persons.remove(person)
+            },
+          },
+          "RM"
+        )
+      )
+    }
+
+    const App = () => {
+      const [_, tick] = React.useReducer(reducer, seq)
+
+      React.useEffect(() => {
+        graph.persons.on("add add.range remove remove.range", tick)
+        return () => {
+          graph.persons.off("add add.range remove remove.range", tick)
+        }
+      }, [])
+
+      return h(
+        "ul",
+        {},
+        ...graph.persons.map((p) => {
+          return h(Item, { person: p, key: p.id })
+        })
+      )
+    }
+
+    ReactDOM.render(React.createElement(App), root)
   }
 }
 
@@ -437,6 +666,9 @@ App()
  *
  *
  *
+ * TODO:
+ *
+ * 1. remove a container
  *
  *
  *
